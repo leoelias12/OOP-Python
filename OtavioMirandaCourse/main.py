271dcaa91b845a5c4a7c088e2acebeb3958a8b3c
Leonardo <leonardo.felias@hotmail.com>
2024-01-26 14:24:10 +0100

    Incremented Person Object
    created a few more methods

---
+++ OtavioMirandaCourse/main.py
@@ -0,0 +1,13 @@
+from person import Person
+
+p1 = Person('Leonardo', 26)
+
+p1.eat('apple')
+p1.eat('Banana')
+p1.speak()
+p1.stop_eating()
+p1.speak('python')
+p1.eat('meat')
+p1.stop_speaking()
+p1.eat('chicken')
+p1.eat('pasta')
---
+++ OtavioMirandaCourse/person.py
@@ -0,0 +1,47 @@
+class Person:
+    current_year = 2023
+
+    def __init__(self, name, age, eating=False, speaking=False):
+        self.name = name
+        self.age = age
+        self.eating = eating
+        self.speaking = speaking
+
+    def get_birth_year(self):
+        birth_year = Person.current_year - self.age
+        return birth_year
+
+    def eat(self, food):
+        if self.speaking:
+            print(f'{self.name} can\'t eat while speaking.')
+        else:
+            if self.eating:
+                print(f'{self.name} is already eating.')
+            else:
+                print(f'{self.name} is eating {food}')
+                self.eating = True
+
+    def stop_eating(self):
+        if not self.eating:
+            print(f'{self.name} is not eating.')
+        else:
+            self.eating = False
+            print(f'{self.name} stopped eating.')
+
+    def speak(self, subject='random things'):
+        if self.speaking:
+            print(f'{self.name} is already speaking.')
+            return
+        else:
+            if self.eating:
+                print(f'{self.name} can\'t speak while eating')
+            else:
+                print(f'{self.name} is speaking about {subject}')
+                self.speaking = True
+
+    def stop_speaking(self):
+        if not self.speaking:
+            print(f'{self.name} is not speaking.')
+        else:
+            self.speaking = False
+            print(f'{self.name} stopped speaking.')
